bda6f0ffdd81af05f8e7880281830c69a274adea
Allen S <allenmshultz@gmail.com>
2020-03-29 19:51:02 -0400

    fixed an issue using the max review character count instead of token count

--- model_functions.py
+++ model_functions.py
@@ -16,27 +16,27 @@ USAGE: ./model_functions.py, though these functions are intended to be imported 
 '''
 Builds the model to be used for sentiment classification. 
 	inputs:
-      - lengthOfInput: length of maximum input
+      - intputTokenCount: length of maximum input
       - lengthOfLabels: number of possible values for the labels
    return:
       - model: keras model, the model compiled
 '''
-def build_model(lengthOfInput: int, lengthOfLabels: int) -> Model:
-   print('Building keras Sequential model with length of input: {}, labels length: {}'.format(lengthOfInput, 
+def build_model(intputTokenCount: int, lengthOfLabels: int) -> Model:
+   print('Building keras Sequential model with length of input: {}, labels length: {}'.format(intputTokenCount, 
                                                                                               lengthOfLabels))
    # Build the embedding layer which creates the weight matrix of (vocab_size) x (embedding dimension) and then indexes
    # this weight matrix. Note that in read_dataset_into_memory() we only Tokenized the 10000 most common words so the 
    # embedding layer shall take that into account.
-   embeddingLayer = layers.Embedding(input_dim=10000, output_dim=128, input_length=lengthOfInput)
-   sequenceInput = layers.Input(shape=(lengthOfInput,), dtype="int32")
+   embeddingLayer = layers.Embedding(input_dim=10000, output_dim=128, input_length=intputTokenCount)
+   sequenceInput = layers.Input(shape=(intputTokenCount,), dtype="int32")
    embeddedSequences = embeddingLayer(sequenceInput)
    
    # Build the model.
-   layerInstance = layers.Conv1D(128, 5, activation='relu')(embeddedSequences)
-   layerInstance = layers.MaxPooling1D(5)(layerInstance)
-   layerInstance = layers.Conv1D(128, 5, activation='relu')(layerInstance)
-   layerInstance = layers.MaxPooling1D(5)(layerInstance)
-   layerInstance = layers.Conv1D(128, 5, activation='relu')(layerInstance)
+   layerInstance = layers.Conv1D(128, 1, activation='relu')(embeddedSequences)
+   layerInstance = layers.MaxPooling1D(1)(layerInstance)
+   layerInstance = layers.Conv1D(128, 1, activation='relu')(layerInstance)
+   layerInstance = layers.MaxPooling1D(1)(layerInstance)
+   layerInstance = layers.Conv1D(128, 1, activation='relu')(layerInstance)
    
    layerInstance = layers.LSTM(64, dropout=0.2)(layerInstance)
    layerInstance = tf.expand_dims(layerInstance, axis=-1)
